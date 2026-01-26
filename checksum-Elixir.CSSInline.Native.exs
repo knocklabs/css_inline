1cbb1dd9a5a16b8c8c867a9e0ca13cccc94b92fe
Jared Dellitt <jareddellitt@users.noreply.github.com>
2026-01-26 10:49:05 -0600

    chore(PLA-2124): generate new checksum file (#7)

--- checksum-Elixir.CSSInline.Native.exs
+++ checksum-Elixir.CSSInline.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "libcss_inline_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c85641489a20c51bbcf97070ed665c97057198b6febea9ea898705675ebe8f1f",
-  "libcss_inline_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:43beaab996341f0bc726dceb62c6ed911fa66ba3fb3a02566051fee40135b275",
-  "libcss_inline_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:3e2aabb6be61be59383de559f6a9bbf2d5e5b53e912d70d9c1942b3e1171d7fb",
-  "libcss_inline_nif-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:17e0297c4d90475b01cd11b679ef79cd392a3d82c4a3e244bfb328b46797311a",
-  "libcss_inline_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:eb7e7a62afe2f84ce9daf0a2ed6225714b336fad3506e307813b247d8d78ac28",
-  "libcss_inline_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:03b99fc09b95b4ec453e8df2cd9c70b7ecbd5f83f852d18300bd5ad408158d94",
+  "libcss_inline_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ad2dc28d50bd804758b4b41ac7669c64e66b7fb7d22ca668ddacd9176c9d7063",
+  "libcss_inline_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c328549469179f76282da6299375c45e0bb8d4cdb3eb7e2435e2dc396e43ed42",
+  "libcss_inline_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:cb43145961f8b254fef9f7b185d05d1c22c6c28457ba8ebc701032c85e1ea3a4",
+  "libcss_inline_nif-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:707bd9f0eb2ff5ee7cd03944aedf54ef6d7979110a6c0985cd1cb972caa6abad",
+  "libcss_inline_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4020cf62a46c4d7e0ac77c2c7fec3b4c61ec5af09a1751045fc2d4d8293d025a",
+  "libcss_inline_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:b4bb7934c0a1619f4457f48e4c1db4e967cab926845c85996827954c2bf887a4",
 }
