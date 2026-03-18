c158d5daaf15ad4517ff95b282bb306641a84ec2
Jared Dellitt <jared@knock.app>
2026-03-18 15:15:36 -0500

    chore(PLA-2392): add updated checksums & lock file

--- checksum-Elixir.CSSInline.Native.exs
+++ checksum-Elixir.CSSInline.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "libcss_inline_nif-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2f23bd51961e315aa6d10ec3fc6d592848d023abc6a7a83438c95411860e53c3",
-  "libcss_inline_nif-v0.1.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:df4ae74cf06d2e6c03162d152eeb97871edc565d032df5e5f3654cc51368c385",
-  "libcss_inline_nif-v0.1.2-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:166d4346c979396a60840d034e84edcc3f58c3a69b7af03949d4ffe76b1faf6c",
-  "libcss_inline_nif-v0.1.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:1740fd8198612a65069b27907b0e51e63f11f4058cd24935feb2837a51d2dc15",
-  "libcss_inline_nif-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b43e449c012f0c660eb65c9f433eb8834436de5034ac8a889007284e99a98fe1",
-  "libcss_inline_nif-v0.1.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:1ea9186faf5c84ea30bca300213b8a8a2da4c1f2c722fb56307c4f797aeae2d4",
+  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1f24a7bc728625fc333bfa094893e8c0428ab54079c08742c986666b24a6f460",
+  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:2767486061356eb79c50703de90fe1fe8081aebcf8f03580cc86a97af8899e79",
+  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:586963f4690389593d26fec8563b7e95cea063edf2e5ce8bce7acbfefbbe3ac6",
+  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:d624fcf71a0c728deea5c5fd3e97cb8cc20f70ee76be95391c1e687d57749ed5",
+  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c88dad6f3f4b32dfed96b2a9830d26dbae80c407743541a1377724d9c14f7fb2",
+  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:e949657caa05f7d751724c912c29a08d090dea4ebe6fae0d57fe4040b16f53ec",
 }
