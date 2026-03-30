002c10bee9da40d3c6277ed067e5b6d6ea2b6279
Jared Dellitt <jared@knock.app>
2026-03-30 08:13:46 -0500

    chore: add updated checksums

--- checksum-Elixir.CSSInline.Native.exs
+++ checksum-Elixir.CSSInline.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1f24a7bc728625fc333bfa094893e8c0428ab54079c08742c986666b24a6f460",
-  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:2767486061356eb79c50703de90fe1fe8081aebcf8f03580cc86a97af8899e79",
-  "libcss_inline_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:586963f4690389593d26fec8563b7e95cea063edf2e5ce8bce7acbfefbbe3ac6",
-  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:d624fcf71a0c728deea5c5fd3e97cb8cc20f70ee76be95391c1e687d57749ed5",
-  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c88dad6f3f4b32dfed96b2a9830d26dbae80c407743541a1377724d9c14f7fb2",
-  "libcss_inline_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:e949657caa05f7d751724c912c29a08d090dea4ebe6fae0d57fe4040b16f53ec",
+  "libcss_inline_nif-v0.2.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:233602185d9ad60471820e5254abb1cc3e83b92aeaaea09a85d7b3de802716c3",
+  "libcss_inline_nif-v0.2.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:de834ac713ebad328573346859adaf405d7b1ad16ab21392937b9a283f4ab705",
+  "libcss_inline_nif-v0.2.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:83d3cfeb6fee936abd0b0de735d7d240193b9a4335003099b3ea4072cf3e77d0",
+  "libcss_inline_nif-v0.2.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:7973c666cceb9c8ef84b48d2e9dd9ae2d472a64be42b3f81eddaa34c9109c3b1",
+  "libcss_inline_nif-v0.2.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:143575ec3bfff7efe27068ca2e681043e1335c74c32cc77e9ad89c6ad965551e",
+  "libcss_inline_nif-v0.2.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:e0b4a0c7748940aea87d2598a7f85a14d2d8a6697880aa80b763696f60984d8b",
 }
